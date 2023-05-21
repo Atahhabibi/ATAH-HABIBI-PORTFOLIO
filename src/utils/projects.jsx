4fc2f4fbfed6085ba656b5aafd66447e977b976c
atah <habibiatah@gmail.com>
2023-05-21 08:08:39 -0700

    completed

---
+++ src/utils/projects.jsx
@@ -0,0 +1,128 @@
+import backroadTourImg from '../images/projects/BackroadTour.png';
+import NokuSocialImg from '../images/projects/NukuSocial.png';
+import CaraStoreImg from '../images/projects/karaStore.png';
+import EgadorEducationImg from '../images/projects/karaStore.png';
+import CocktailImg from '../images/projects/Cocktail.png';
+import SearchImg from '../images/projects/Search.png';
+import StoreImg from '../images/projects/Store.png';
+
+
+export const projectsData=[
+
+    {
+        name:'REACT',
+        projects:[
+            {
+                id:1,
+                name:'WatchMovie',
+                liveLink:'',
+                githhubLink:'',
+                img:'',
+            },
+            {
+                id:2,
+                name:'Yutube Clone',
+                liveLink:'',
+                githhubLink:'',
+                img:'',
+            },
+            {
+                id:3,
+                name:'Google Search Clone',
+                liveLink:'',
+                githhubLink:'',
+                img:'',
+            },
+            {
+                id:4,
+                name:'Jobster',
+                liveLink:'',
+                githhubLink:'',
+                img:'',
+            },
+            {
+                id:5,
+                name:'Comfy Store',
+                liveLink:'',
+                githhubLink:'',
+                img:'',
+            },
+        ]
+    },
+
+    {
+        name:'HTML/CSS',
+        projects:[
+            {
+                id:1,
+                name:'BackRoad Tour Company',
+                liveLink:'https://company-backroad-tours.netlify.app/',
+                githhubLink:'https://github.com/Atahhabibi/TOUR-COMPANY-PROJECT',
+                img:backroadTourImg,
+            },
+            {
+                id:2,
+                name:'Noku Social App',
+                liveLink:'https://nokusocial.netlify.app/',
+                githhubLink:'https://github.com/Atahhabibi/SOCIAL-MEDIA-NOKO-SOCIAL',
+                img:NokuSocialImg,
+            },
+            {
+                id:3,
+                name:'Portfolio Website',
+                liveLink:'',
+                githhubLink:'',
+                img:"",
+            },
+            {
+                id:4,
+                name:'cara shopping online',
+                liveLink:'https://shopping-with-us.netlify.app/index.html',
+                githhubLink:'https://github.com/Atahhabibi/KARA-E-COMMERCE-WEBSITE',
+                img:CaraStoreImg,
+            },
+            {
+                id:5,
+                name:'Egator education',
+                liveLink:'https://egator-education.netlify.app/index.html',
+                githhubLink:'https://github.com/Atahhabibi/EGATOR-EDUCATION-WEBSITE',
+                img:EgadorEducationImg,
+            },
+            
+        ]
+    },
+
+    {
+        name:'JAVASCRIPT',
+        projects:[
+            {
+                id:1,
+                name:'Cocktial Drink Shop',
+                liveLink:'https://drink-site-project.netlify.app/index.html',
+                githhubLink:'https://github.com/Atahhabibi/ATAH-PRACTICE/tree/main/24-Coktails',
+                img:CocktailImg,
+            },
+            {
+                id:2,
+                name:'Search Wikipedia Clone',
+                liveLink:'https://wikipedia-search-site.netlify.app/',
+                githhubLink:'https://github.com/Atahhabibi/ATAH-PRACTICE/tree/main/28-Wikipedia',
+                img:SearchImg,
+            },
+            {
+                id:3,
+                name:'comfy furniture store',
+                liveLink:'https://comfy-online-store.netlify.app/index.html',
+                githhubLink:'https://github.com/Atahhabibi/ATAH-PRACTICE/tree/main/29-comfy-store',
+                img:StoreImg,
+            },
+         
+            
+        ]
+    },
+
+
+    
+
+    
+]
